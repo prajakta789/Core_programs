2bc8d945969ede2de1daf92a315f46f4e881b065
prajakta <prajaktamahajan789@gmail.com>
2021-09-20 16:18:20 +0530

    power of two programe

---
+++ PowerOfTwo.java
@@ -0,0 +1,28 @@
+package com.basic_core_programe;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+	 static final int MAX_VALUE = 32;
+	    static final int MIN_VALUE = 0;
+
+	    public static void main(String[] args) {
+	        //Variables
+	        int number;
+	        int power;
+	        System.out.print("Enter a number to Print 2 power Table : ");
+	        Scanner scanner = new Scanner(System.in);
+	        number = scanner.nextInt();
+	        // checking the number in the 0 to 32 range , to not encounter overflow
+	        if (number >= MIN_VALUE && number < MAX_VALUE) {
+	            for (int i = 0; i < number; i++) {
+	                power = (int) Math.pow(2, i);
+	                System.out.println("2" + "^" + i + "=" + power);
+	            }
+	        }
+	    }
+  }
+	            
+
+
+
